a792e9ac379f1489e263fef3ade9ad86f23c6bc4
fleetapps <andrew@fleet.ke>
2026-04-10 13:14:22 +0300

    Update __manifest__.py

--- iframe_allow/__manifest__.py
+++ iframe_allow/__manifest__.py
@@ -1,6 +1,8 @@
 {
     'name': 'Allow iFrame Embedding',
     'version': '1.0',
+    'author': 'ODIN',
+    'license': 'LGPL-3',
     'depends': ['web'],
     'installable': True,
     'post_load': 'apply_iframe_patch',
