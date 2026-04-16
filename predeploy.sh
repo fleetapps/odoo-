a288c4786bdb3b6fb2f349f71e78c46be3d24061
fleetapps <andrew@fleet.ke>
2026-04-16 22:52:01 +0300

    Update predeploy.sh

--- predeploy.sh
+++ predeploy.sh
@@ -1,3 +1,2 @@
 mkdir -p /var/lib/odoo/custom_addons
-cp -r /odoo_modules/odin_sandbox/. /var/lib/odoo/custom_addons/odin_sandbox/
-cp -r /odoo_modules/iframe_allow/. /var/lib/odoo/custom_addons/iframe_allow/
+mkdir -p /var/lib/odoo/.local/share/Odoo/filestore
